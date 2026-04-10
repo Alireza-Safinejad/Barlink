f091802eb6afe58bc689650c1f683c57bdeddd70
Alireza-Safinejad <118644550+Alireza-Safinejad@users.noreply.github.com>
2026-04-10 22:01:35 +0330

    Create data.js

---
+++ data.js
@@ -0,0 +1,42 @@
+let loads=[
+
+{
+
+id:1,
+
+origin:"سیرجان",
+
+destination:"بندرعباس",
+
+price:32000000,
+
+cargo:"سنگ آهن",
+
+lat:29.45,
+
+lng:55.68
+
+},
+
+{
+
+id:2,
+
+origin:"کرمان",
+
+destination:"یزد",
+
+price:25000000,
+
+cargo:"سیمان",
+
+lat:30.29,
+
+lng:57.06
+
+}
+
+]
+
+
+let drivers=[]
